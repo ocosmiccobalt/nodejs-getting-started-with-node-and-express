964248387dbf4faca0bf6c7e89bffb42b29f7ed6
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-06-28 00:18:01 +0300

    Clean up (remove old code)

--- app.js
+++ app.js
@@ -11,19 +11,3 @@ app.get('/', (req, res) => {
 }); // localhost:3000/
 
 app.listen(3000);
-
-// const http = require('http');
-
-// function handleRequest(request, response) {
-//   if (request.url === '/currenttime') {
-//     response.statusCode = 200;
-//     response.end('<h1>' + new Date().toISOString() + '</h1>');
-//   } else if (request.url === '/') {
-//     response.statusCode = 200;
-//     response.end('<h1>Hello World!</h1>');
-//   }
-// }
-
-// const server = http.createServer(handleRequest);
-
-// server.listen(3000);
